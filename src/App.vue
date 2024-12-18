72e08b43d8f93abbc990e51821054cb80710f00f
Pasquale Di Matteo <pasqualedm94@gmail.com>
2024-12-18 15:09:44 +0100

    added header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script>
+import AppHeader from "./components/AppHeader.vue";
 export default {
-
+  components: { AppHeader }
 };
 </script>
 
 <template>
-  <h1>Hello</h1>
+  <AppHeader />
+  <main class="container my-3">
+    <h1>Hello</h1>
+  </main>
 </template>
 
 <style></style>
